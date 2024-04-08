330cd1fdade192a65e5f78fee43eaf6b02d9b00e
wkdtpzld <87063105+wkdtpzld@users.noreply.github.com>
2024-04-08 18:59:20 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: { appDir: true }, // Next.js v13 app directory 사용 여부
+  output: 'standalone',  // 여기서 standalone 폴더를 사용한다고 해준다.
+};
 
 export default nextConfig;
